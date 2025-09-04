aec066d8417cd0b4e93a8caa94a21119763605b6
josephats1 <bjerrykibs@gmail.com>
2025-09-04 17:42:54 +0300

    changed index folder, corrected port to 5500 from 3000

--- 340-starter-main/server.js
+++ 340-starter-main/server.js
@@ -10,23 +10,29 @@ const expressLayouts = require("express-ejs-layouts")
 const env = require("dotenv").config()
 const app = express()
 const static = require("./routes/static")
-
-
-app.use(static)
 /* ***********************
- * Routes
+ * View Engine and Templates
  *************************/
 app.set("view engine", "ejs")
 app.use(expressLayouts)
 app.set("layout", "./layouts/layout") // not at views root
+
+/* ***********************
+ * Routes
+ *************************/
 app.use(static)
+
+// Index route
+app.get("/", (req, res) => {
+  res.render("index", {title: "Home"})
+})
 
 /* ***********************
  * Local Server Information
  * Values from .env (environment) file
  *************************/
-const port = process.env.PORT
-const host = process.env.HOST
+const port = process.env.PORT || 5500 // same as of the env
+const host = process.env.HOST || "localhost" // Added default host
 
 /* ***********************
  * Log statement to confirm server operation
