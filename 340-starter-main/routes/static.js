7c7e16a94f294c15d98e73a80052038eccdc1046
josephats1 <bjerrykibs@gmail.com>
2025-09-04 16:52:33 +0300

    first commit

---
+++ 340-starter-main/routes/static.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const router = express.Router();
+
+// Static Routes
+// Set up "public" folder / subfolders for static files
+router.use(express.static("public"));
+router.use("/css", express.static(__dirname + "public/css"));
+router.use("/js", express.static(__dirname + "public/js"));
+router.use("/images", express.static(__dirname + "public/images"));
+
+router.get("/about", (req, res) => {
+  res.render("index", { title: "About" })
+})
+module.exports = router;
+
+
+
